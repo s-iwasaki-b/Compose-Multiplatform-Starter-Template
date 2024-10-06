ee7202528f8ee9d6f2c841477c9dd8873b554c7a
Seiya Iwasaki <seiya.iwasaki@dely.jp>
2024-10-06 14:53:09 +0900

    remove unused component

--- composeApp/src/commonMain/kotlin/org/starter/project/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package org.starter.project
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/org/starter/project/Platform.kt
+++
@@ -1,7 +0,0 @@
-package org.starter.project
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform

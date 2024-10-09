f60c5999cb2c05814e6c5a42d95bf756bf211412
Seiya Iwasaki <seiya.iwasaki@dely.jp>
2024-10-10 01:17:27 +0900

    fix minor issue

---
+++ composeApp/src/iosMain/kotlin/org/starter/project/app/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.starter.project.app
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { MainApp() }
--- module/core/src/commonMain/kotlin/org/starter/project/core/api/ApiConfig.kt
+++ module/core/src/commonMain/kotlin/org/starter/project/core/api/ApiConfig.kt
@@ -1,7 +1,5 @@
 package org.starter.project.core.api
 
-import io.ktor.http.HttpMessageBuilder
-
 internal object ApiConfig {
     const val API_BASE_URL = "https://zenn.dev/"
 }

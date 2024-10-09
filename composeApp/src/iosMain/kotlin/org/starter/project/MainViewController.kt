94532840cbcb9be12407d21c48fc0a97e3644fec
Seiya Iwasaki <seiya.iwasaki@dely.jp>
2024-10-10 01:17:36 +0900

    refactor

--- composeApp/src/iosMain/kotlin/org/starter/project/MainViewController.kt
+++
@@ -1,6 +0,0 @@
-package org.starter.project
-
-import androidx.compose.ui.window.ComposeUIViewController
-import org.starter.project.app.MainApp
-
-fun MainViewController() = ComposeUIViewController { MainApp() }

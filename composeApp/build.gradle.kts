d52eafdaf71c0bb152efe2032f4b88c1d0f01c8e
Seiya Iwasaki <seiya.iwasaki@dely.jp>
2024-10-10 00:29:05 +0900

    fix manifest

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -32,7 +32,6 @@ kotlin {
     
     sourceSets {
         androidMain.dependencies {
-            implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
         }
         commonMain.dependencies {
---
+++ composeApp/src/androidMain/kotlin/org/starter/project/app/MainActivity.kt
@@ -0,0 +1,27 @@
+package org.starter.project.app
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT, Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT, Color.TRANSPARENT
+            )
+        )
+
+        setContent {
+            MainApp()
+        }
+    }
+}

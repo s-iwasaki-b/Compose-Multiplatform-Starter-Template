a5f0ef1c81c51ef7c173f559394ffbeadc128489
Seiya Iwasaki <seiya.iwasaki@dely.jp>
2024-10-12 16:38:11 +0900

    remove unused account module

--- composeApp/src/commonMain/kotlin/org/starter/project/di/Koin.kt
+++ composeApp/src/commonMain/kotlin/org/starter/project/di/Koin.kt
@@ -4,12 +4,8 @@ import org.koin.core.KoinApplication
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
-import org.starter.project.data.account.repository.AccountRepositoryImpl
-import org.starter.project.data.repository.AccountRepository
 import org.starter.project.data.repository.ZennRepository
 import org.starter.project.data.zenn.repository.ZennRepositoryImpl
-import org.starter.project.domain.account.AccountServiceImpl
-import org.starter.project.domain.service.AccountService
 import org.starter.project.domain.service.ResultHandler
 import org.starter.project.domain.service.ZennService
 import org.starter.project.domain.zenn.ZennServiceImpl
@@ -17,13 +13,11 @@ import org.starter.project.feature.home.HomeScreenViewModel
 
 fun koin(): KoinApplication.() -> Unit = {
     val repositoryModule = module {
-        singleOf<AccountRepository>(::AccountRepositoryImpl)
         singleOf<ZennRepository>(::ZennRepositoryImpl)
     }
 
     val serviceModule = module {
         single { ResultHandler() }
-        single<AccountService> { AccountServiceImpl(get(), get()) }
         single<ZennService> { ZennServiceImpl(get(), get()) }
     }
 
--- module/data/account/src/commonMain/kotlin/org.starter.project.data/account/repository/AccountRepositoryImpl.kt
+++
@@ -1,6 +0,0 @@
-package org.starter.project.data.account.repository
-
-import org.starter.project.data.repository.AccountRepository
-
-class AccountRepositoryImpl : AccountRepository {
-}

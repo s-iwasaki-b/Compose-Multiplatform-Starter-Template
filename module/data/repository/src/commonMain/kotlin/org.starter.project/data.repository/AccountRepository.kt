4239f938c189380d347ee63f4e8061462474a5eb
Seiya Iwasaki <seiya.iwasaki@dely.jp>
2024-10-12 16:44:19 +0900

    remove unused interface

--- module/data/repository/src/commonMain/kotlin/org.starter.project/data.repository/AccountRepository.kt
+++
@@ -1,5 +0,0 @@
-package org.starter.project.data.repository
-
-interface AccountRepository : Repository {
-
-}
--- module/domain/service/src/commonMain/kotlin/org.starter.project.domain/service/AccountService.kt
+++
@@ -1,5 +0,0 @@
-package org.starter.project.domain.service
-
-interface AccountService : Service {
-
-}

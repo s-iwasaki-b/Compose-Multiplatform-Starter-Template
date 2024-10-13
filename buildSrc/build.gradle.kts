d4240b83c9d10c1f4f4400c46609545cf01c297f
Seiya Iwasaki <seiya.iwasaki@dely.jp>
2024-10-13 22:11:14 +0900

    add gradle task to change package name

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,3 +5,27 @@ plugins {
 repositories{
     mavenCentral()
 }
+
+tasks.register("changePackageName") {
+    val oldPackageName = "org.starter.project"
+    val newPackageName: String? = project.findProperty("newPackageName") as String?
+    val fileExtensions = listOf("kt", "xml", "xcconfig")
+
+    doLast {
+        if (newPackageName == null) {
+            println("Please provide the new package name using -PnewPackageName=...")
+            return@doLast
+        }
+
+        project.projectDir.walkTopDown().forEach { file ->
+            if (file.isFile && fileExtensions.any { file.name.endsWith(it) }) {
+                val fileText = file.readText()
+                if (fileText.contains(oldPackageName)) {
+                    val newText = fileText.replace(oldPackageName, newPackageName)
+                    file.writeText(newText)
+                    println("Updated package name in ${file.path}")
+                }
+            }
+        }
+    }
+}

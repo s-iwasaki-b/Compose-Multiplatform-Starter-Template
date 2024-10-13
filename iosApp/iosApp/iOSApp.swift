68e7481ca545060d03b616aa37c76a804210f0b8
Seiya Iwasaki <seiya.iwasaki@dely.jp>
2024-10-13 21:58:16 +0900

    fix func name

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -15,7 +15,7 @@ struct iOSApp: App {
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         #if DEBUG
-            NapierProxyKt.initNapier()
+        NapierProxyKt.doInitNapier()
         #endif
         return true
     }

c72bf3b11726d996cd6578fcdf5a03c3c176ade2
Seiya Iwasaki <seiya.iwasaki@dely.jp>
2024-10-13 22:24:55 +0900

    fix task

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,3 +12,27 @@ plugins {
     alias(libs.plugins.ktorfit) apply false
     alias(libs.plugins.mokkery) apply false
 }
+
+tasks.register("changePackageName") {
+    val oldPackageName = PACKAGE_NAME
+    val newPackageName: String? = project.findProperty("newPackageName") as String?
+    val fileExtensions = listOf("kt", "xml", "xcconfig")
+
+    doLast {
+        if (newPackageName == null) {
+            throw GradleException("Error: Please provide the new package name using -PnewPackageName=...")
+        }
+
+        println("Changing package name from $oldPackageName to $newPackageName")
+        project.projectDir.walkTopDown().forEach { file ->
+            if (file.isFile && fileExtensions.any { file.name.endsWith(it) }) {
+                val fileText = file.readText()
+                if (fileText.contains(oldPackageName)) {
+                    val newText = fileText.replace(oldPackageName, newPackageName)
+                    file.writeText(newText)
+                    println("Updated package name in ${file.path}")
+                }
+            }
+        }
+    }
+}

d29ecd953c3b3d79a7f069b2f3beefcb181b5ed3
Seiya Iwasaki <seiya.iwasaki@dely.jp>
2024-10-13 21:38:59 +0900

    rename init napier

--- composeApp/src/iosMain/kotlin/org/starter/project/log/NapierProxy.kt
+++ composeApp/src/iosMain/kotlin/org/starter/project/log/NapierProxy.kt
@@ -3,6 +3,6 @@ package org.starter.project.log
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 
-fun debugBuild() {
+fun initNapier() {
     Napier.base(DebugAntilog())
 }
